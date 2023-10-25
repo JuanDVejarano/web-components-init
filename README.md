41cf38586a6212ca0663c4d5dc676e9035bf3aaf
ToyekaXV <juanjdv_200@hotmail.com>
2023-10-25 15:38:36 -0500

    Templates y shadowDom

---
+++ README.md
@@ -0,0 +1,14 @@
+Para la construccion de web componentes se utilizan 4 web APIs
+
+## 1 HTMLElement
+
+me permite crear un objeto con la estructura neccesaria para inyectar el codigo en el DOM
+en la carpeta **Custom-Elements** podemos ver algunos ejemplos de como usarlo
+
+## 2 Template
+
+Me permite encapsuar el codigo dentro de un document-fragment
+
+## 3 Shadow Dom
+
+Me permite encapsular los componentes para hacer un ecosistema porpio e independiente del elemento
---
+++ shadowDom/my-component.js
@@ -0,0 +1,38 @@
+class myElement extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+  getTemplate() {
+    const template = document.createElement("template");
+    template.innerHTML = `
+        <section>
+            <h2>Hola Mundo</h2>
+            <div>
+                <p>Soy mas texto ejemplo</p>
+            </div>
+        </section>
+        ${this.getStyle()}
+    `;
+    return template;
+  }
+
+  getStyle() {
+    return `
+      <style>
+        h2{
+            color: red;
+        }
+      </style>
+    `;
+  }
+
+  render() {
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+  connectedCallback() {
+    this.render();
+  }
+}
+
+customElements.define("my-element", myElement);
